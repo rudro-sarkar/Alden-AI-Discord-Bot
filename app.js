7ecfdc31fa7af4c86d2de8d1c4acbe33614e081b
rudro-sarkar <rudrosarkar2327fb@gmail.com>
2024-09-27 14:35:44 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 require('dotenv/config');
-require('./keep_alive');
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'templates'));
+
+app.get('/', (req, res) => {
+    res.render('useless');
+});
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+    console.log(`Monitor server started at ${port}`);
+});
 
 const brain = require('brain.js');
 const fs = require('fs');
